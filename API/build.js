2854eb33211154a67fbe9e5671d05d7287ba5da1
itamayo <itamayo@vicomtech.org>
2015-07-30 10:32:09 +0200

    added minimized build file

---
+++ API/build.js
@@ -0,0 +1,13 @@
+({
+    baseUrl: 'js',
+    paths: {
+
+      underscore:'../resources/libs/underscore-min',
+      jquery: '../resources/libs/jquery-2.1.1.min',
+      qrcode:'../resources/libs/qrcode.min',
+      namedwebsockets:'lib/namedwebsockets'
+    },
+    name: "mediascape.uiengine.js",
+    out: "mediascape.uiengine.min.js",
+    removeCombined: true
+})
